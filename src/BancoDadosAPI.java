b463efaecf801fa5622cf4ab639bbaef5c7d47ab
furtadcleticia <leticiafurtado211205@gmail.com>
2025-07-01 09:27:57 -0300

    Metodo Mudar_status e atualização de inserir tarefas

--- src/BancoDadosAPI.java
+++ src/BancoDadosAPI.java
@@ -15,6 +15,8 @@ public class BancoDadosAPI {
             "concluida BOOLEAN NOT NULL," +
             "data_de_alteracao TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
             ")";
+    private static final String MUDAR_STATUS = "UPDATE tarefas SET concluida = ?, data_de_alteracao = CURRENT_TIMESTAMP WHERE task = ?";
+
 
     public BancoDadosAPI() {
         this.db = MyJDBC.pegarConexao();
@@ -64,6 +66,14 @@ public class BancoDadosAPI {
 
 
     public boolean inserirTarefa(String texto, Boolean concluida) throws SQLException {
+        Optional<List<Tarefa>>tarefas= selecionarTodasTarefas();
+        if (tarefas.isPresent()){
+            for (Tarefa tarefa : tarefas.get())
+                if (tarefa.task.equals(texto)) {
+                    System.out.println("Essa tarefa já existe");
+                    return false;
+                }
+        }
         PreparedStatement comando = db.get().prepareStatement(INSERIR_TAREFA_SQL);
         comando.setString(1, texto);
         comando.setBoolean(2, concluida);
@@ -76,5 +86,29 @@ public class BancoDadosAPI {
         }
         System.out.println("Tarefa Criada com sucesso!");
         return true;
+    }
+
+
+    public boolean MudarStatus(Tarefa tarefa) throws SQLException {
+        boolean status = true;
+
+        if (tarefa.concluida){
+            status= false;
+        }
+        PreparedStatement comando = db.get().prepareStatement(MUDAR_STATUS);
+        comando.setBoolean(1, status);
+        comando.setString(2, tarefa.task);
+
+        try {
+            comando.executeUpdate();
+            comando.close();
+        } catch (SQLException e) {
+            System.out.println("Erro ao mudar status: " + e.getMessage());
+            return false;
+        }
+        System.out.println("Status concluído!");
+        return true;
+
+
     }
 }
