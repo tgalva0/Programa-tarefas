a3fabce636d2f8f914f12b170041f2cc2c295d94
Thiago Galvao Ferreira <thigalvao2005@gmail.com>
2025-07-15 12:03:22 -0300

    atualização da interface Gráfica

--- src/main/TarefasApp.java
+++ src/main/TarefasApp.java
@@ -1,17 +1,53 @@
 package main;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 public class TarefasApp extends Application {
     @Override
     public void start(Stage palco) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("GUI/view/Main.fxml"));
         Parent raiz = loader.load();
-        palco.setScene(new Scene(raiz));
+        Scene cena = new Scene(raiz);
+        cena.getStylesheets().add(getClass().getResource("GUI/view/style.css").toExternalForm());
+        Button botao = (Button) raiz.lookup(".adicionar-button");
+
+        botao.setOnMouseEntered((event) -> {
+            Timeline timeline = new Timeline(
+                    new KeyFrame(Duration.seconds(0.3),
+                            new KeyValue(botao.backgroundProperty(),
+                                    new Background(new BackgroundFill(Color.web("#044787"), new CornerRadii(10), Insets.EMPTY))))
+            );
+            timeline.play();
+        });
+
+        botao.setOnMouseExited((event) -> {
+            Timeline timeline = new Timeline(
+                    new KeyFrame(Duration.seconds(0.3),
+                            new KeyValue(botao.backgroundProperty(),
+                                    new Background(new BackgroundFill(Color.web("#3a3d40"), new CornerRadii(10), Insets.EMPTY))))
+            );
+            timeline.play();
+        });
+
+        palco.initStyle(StageStyle.DECORATED);
+        palco.getIcons().add(new Image(getClass().getResourceAsStream("GUI/view/icon.png")));
+        palco.setScene(cena);
         palco.setTitle("Bloco de Tarefas");
         palco.show();
     }
